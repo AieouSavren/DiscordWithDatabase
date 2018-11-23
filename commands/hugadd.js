46f6091d8bd070224acf8ded4255a15032160080
Daniel <aieousavren@yahoo.com>
2018-11-22 21:53:46 -0500

    Fixes whitespace in hugadd and hugdelete.

--- commands/hugadd.js
+++ commands/hugadd.js
@@ -14,64 +14,62 @@ HugAdverbs[8] = "zealously";
 HugAdverbs[9] = "ferociously";
 
 module.exports = {
-    name: 'hugadd',
+	name: 'hugadd',
 	aliases: ['hugsadd' , 'addhug' , 'addhugs'],
 	cooldown: 5,
-    description: 'You could use more types of hugs!',
-    execute(msg, args, db) {
-		 var author = msg.author; 
-		  var num = 0;
-		   var returnmessage = "";
-		  
-		  if (!args.length) {
+	description: 'You could use more types of hugs!',
+	execute(msg, args, db) {
+		var author = msg.author; 
+		var num = 0;
+		var returnmessage = "";
+		
+		if (!args.length) {
 			return msg.channel.send("Add an adverb to SAI's database of hugs!");
 			
 			//maybe pm a list of current adverbs o3o? It could be a very very long list... 
-		  } 
-		  else
-		  {
-			  
-				   if (!db) {
+		} 
+		else
+		{
+			
+			if (!db) {
 				//initDb(function(err){});
-				  for(i3 = 0; i3<args.length-1;i3++)
-				  {
-				  returnmessage += args[i3] + ' ';
-				  }
-				  
-				    returnmessage += args[args.length-1];
-				  
-				   msg.channel.send('SAI cannot remember to hug "' + returnmessage + '" right now.' ); 
+				for(i3 = 0; i3<args.length-1;i3++)
+				{
+					returnmessage += args[i3] + ' ';
+				}
+			
+				returnmessage += args[args.length-1];
+			
+				msg.channel.send('SAI cannot remember to hug "' + returnmessage + '" right now.' ); 
 				return;
-			  }
-			  if (db) {
-			  try {
-				  
-				  for(i3 = 0; i3<args.length-1;i3++)
-				  {
-				  returnmessage += args[i3] + ' ';
-				  }
-				  
-				    returnmessage += args[args.length];
-				  
+			}
+			
+			if (db) {
+				try {
+					
+					for(i3 = 0; i3<args.length-1;i3++)
+					{
+						returnmessage += args[i3] + ' ';
+					}
+					
+					returnmessage += args[args.length];
+					
 					autoIncrement.getNextSequence(db, 'hugs', function (err, autoIndex) {
 						if (err) throw err;
 						var collection = db.collection('hugs');
 						collection.insert({ _id: autoIndex, adverbs: returnmessage });
+						
 						msg.channel.send('The Sai bot can now hug ' + returnmessage + '!');
 						
 					});
-					}
-					catch (err) {
-						console.log(err);
-					    msg.channel.send('uh oh...');
-					}
-					
-			  
-			  }
+				} catch (err) {
+					console.log(err);
+					msg.channel.send('uh oh...');
+				}
 			
-		  }
-		  
-       
-		 
-    },
+			}
+			
+		}
+		
+	},
 };
--- commands/hugdelete.js
+++ commands/hugdelete.js
@@ -14,85 +14,79 @@ HugAdverbs[8] = "zealously";
 HugAdverbs[9] = "ferociously";
 
 module.exports = {
-    name: 'hugdelete',
+	name: 'hugdelete',
 	aliases: ['deletehugs', 'deletehug', 'hugsdelete', 'hugdelete', 'hugremove'],
 	cooldown: 5,
-    description: 'You could use a hug!',
-    execute(msg, args, db) {
-		 var author = msg.author; 
-		  var num = 0;
-		   var returnmessage = "";
-		  
-		  if (!args.length) {
+	description: 'You could use a hug!',
+	execute(msg, args, db) {
+		var author = msg.author; 
+		var num = 0;
+		var returnmessage = "";
+		
+		if (!args.length) {
 			msg.channel.send('Give me an adverb to delete from the list...');
-		  } 
-		  else
-		  {
-			  
-			  if (!db) {
-			  msg.channel.send('SAI is very forgetful today.');
-			  }
-			  if (db) {
+		}
+		else
+		{
+			if (!db) {
+				msg.channel.send('SAI is very forgetful today.');
+			}
+			if (db) {
 				
-								
 				try {
 					
 					for(i3 = 0; i3<args.length-1;i3++)
 					{
-					  returnmessage += args[i3] + ' ';
-					}	
-						
+						returnmessage += args[i3] + ' ';
+					}
+					
 					returnmessage += args[args.length-1];
 					
-						var query = { adverbs: returnmessage };
-
-						db.collection("hugs").remove(query, true, function(err, result) {
+					var query = { adverbs: returnmessage };
+					
+					db.collection("hugs").remove(query, true, function(err, result) {
 						if (err) {
 							throw err;
-							
+						
 							console.log('Something went wrong... maybe that was not an option?');
 							return; //dont sort or change the count if we couldnt remove an element
-							}
-							 msg.channel.send('Yeah, ' + returnmessage + ' was not a good way to hug someone.');
-						});
-						  
+						}
+						msg.channel.send('Yeah, ' + returnmessage + ' was not a good way to hug someone.');
+					});
+					
 					var query = { "_id": "hugs"}	  
-						  
-					 //get count and change it to be count - 1
-					  db.collection("counters").find(query, {_id: 0, seq: 1}).toArray(function(err, result) {
+					
+					//get count and change it to be count - 1
+					db.collection("counters").find(query, {_id: 0, seq: 1}).toArray(function(err, result) {
 						if (err) throw err;
 						num = result[0].seq;
 						
-					 
-					db.collection("counters").findOneAndUpdate({"_id": "hugs"}, {$set: {"seq": num-1}},  function(err,doc) {
-					   if (err) { throw err; }
-					   else { console.log("Updated Counter"); }
-					});
+						
+						db.collection("counters").findOneAndUpdate({"_id": "hugs"}, {$set: {"seq": num-1}},  function(err,doc) {
+							if (err) { throw err; }
+							else { console.log("Updated Counter"); }
+						});
 					});
 					for(counter = 1; counter < num-1; counter++)
 					{
-					
+						
 						var query = { "_id": counter}
-					 db.collection("hugs").findOneAndUpdate(query, {$set: {"_id": counter}},  function(err,doc) {
-					   if (err) { throw err; }
-					   else { console.log("Updated id:" + counter); }
+						db.collection("hugs").findOneAndUpdate(query, {$set: {"_id": counter}},  function(err,doc) {
+							if (err) { throw err; }
+							else { console.log("Updated id:" + counter); }
 						});
 					}
 					//change the index of hugs _id
 					
-				
-				 
-				
 				}
-			  catch(err)
-			  {
-				  console.log(err);
-				  msg.channel.send('Something went wrong... maybe that was not an option?');
-			  }
-		  
-		  }
-		  }
-       
-		 
-    },
+				catch(err)
+				{
+					console.log(err);
+					msg.channel.send('Something went wrong... maybe that was not an option?');
+				}
+				
+			}
+		}
+		
+	},
 };
